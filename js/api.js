29d4573d16ad2b0adc6e69cb38f16c938214a596
Pedro Henrique <pedrohg99@yahoo.com>
2026-04-23 12:47:17 -0300

    Cria arquivo api.js que permite a conexao com a api

---
+++ js/api.js
@@ -0,0 +1,13 @@
+const api = {
+    async buscarPensamentos() {
+        try {
+            const response = await fetch ('http://localhost:3000/pensamentos')
+            return response.json()
+        }
+        catch {
+            alert('Erro ao buscar pensamentos')
+        }
+    }
+}
+
+export default api;
