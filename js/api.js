0d22722f1446fbd43c65894dc2a463eb7b751791
Pedro Henrique <pedrohg99@yahoo.com>
2026-04-23 13:22:26 -0300

    Cria nova funcao para realizar a requisicao para salvar os dados inseridos para o banco de dados

--- js/api.js
+++ js/api.js
@@ -7,6 +7,22 @@ const api = {
             alert('Erro ao buscar pensamentos')
             throw error
         }
+    },
+
+    async salvarPensamentos(pensamento) {
+        try {
+            const response = await fetch ('http://localhost:3000/pensamentos', {
+                method: "POST", //envia uma requisição via metodo POST
+                headers: { // indica qual é o tipo de requisição sendo enviada
+                    "Content-Type": "application/json" // conteúdo da requisição do tipo "application/json", ou seja, uma requisição via JSON
+                },
+                body: JSON.stringify(pensamento) // transforma a requisição em string via JSON.stringify
+            })
+            return await response.json()
+        } catch {
+            alert('Erro ao buscar pensamentos')
+            throw error
+        }
     }
 }
 
