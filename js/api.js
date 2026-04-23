4c0188a8af82151ebdb9c32076a829fb2db2fa49
Pedro Henrique <pedrohg99@yahoo.com>
2026-04-23 17:23:15 -0300

    Aplica melhorias no API concatenando a URL como URL_BASE

--- js/api.js
+++ js/api.js
@@ -1,7 +1,9 @@
+const URL_BASE = "http://localhost:3000"
+
 const api = {
     async buscarPensamentos() {
         try {
-            const response = await fetch ('http://localhost:3000/pensamentos')
+            const response = await fetch (`${URL_BASE}/pensamentos`)
             return await response.json()
         } catch {
             alert('Erro ao buscar pensamentos')
@@ -11,7 +13,7 @@ const api = {
 
     async salvarPensamentos(pensamento) {
         try {
-            const response = await fetch('http://localhost:3000/pensamentos', {
+            const response = await fetch(`${URL_BASE}/pensamentos`, {
                 method: "POST", //envia uma requisição via metodo POST
                 headers: { // indica qual é o tipo de requisição sendo enviada
                     "Content-Type": "application/json" // conteúdo da requisição do tipo "application/json", ou seja, uma requisição via JSON
@@ -27,7 +29,7 @@ const api = {
 
     async buscarPensamentosPorId(id) {
         try {
-            const response = await fetch(`http://localhost:3000/pensamentos/${id}`) // busca o pensamento pelo ID
+            const response = await fetch(`${URL_BASE}/pensamentos/${id}`) // busca o pensamento pelo ID
             return await response.json()
         } catch {
             alert('Erro ao buscar pensamento')
@@ -37,7 +39,7 @@ const api = {
 
     async editarPensamentos(pensamento) {
         try {
-            const response = await fetch(`http://localhost:3000/pensamentos/${pensamento.id}`, {
+            const response = await fetch(`${URL_BASE}/pensamentos/${pensamento.id}`, {
                 method: "PUT", //envia uma requisição via metodo PUT, pois será necessário alterar.
                 headers: { // indica qual é o tipo de requisição sendo enviada
                     "Content-Type": "application/json" // conteúdo da requisição do tipo "application/json", ou seja, uma requisição via JSON
@@ -53,7 +55,7 @@ const api = {
     
     async excluirPensamentos(id) {
         try {
-            const response = await fetch(`http://localhost:3000/pensamentos/${id}`, {
+            const response = await fetch(`${URL_BASE}/pensamentos/${id}`, {
                 method: "DELETE", //envia uma requisição via metodo DELETE, pois será necessário remover.
             })
         } catch {
